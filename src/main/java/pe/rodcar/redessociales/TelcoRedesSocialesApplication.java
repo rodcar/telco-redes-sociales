ad623863f20578ba943aa55c5943b99923f8762c
rodcar <ivanrod12@hotmail.com>
2023-01-02 22:12:47 -0500

    commit inicial

---
+++ src/main/java/pe/rodcar/redessociales/TelcoRedesSocialesApplication.java
@@ -0,0 +1,13 @@
+package pe.rodcar.redessociales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoRedesSocialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoRedesSocialesApplication.class, args);
+	}
+
+}

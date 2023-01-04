c9828c011fc2664441af06daa0f0d646f191a2be
rodcar <ivanrod12@hotmail.com>
2023-01-03 19:16:15 -0500

    agrega controlador

---
+++ src/main/java/pe/rodcar/redessociales/controllers/PaqueteController.java
@@ -0,0 +1,36 @@
+package pe.rodcar.redessociales.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pe.rodcar.redessociales.entities.Paquete;
+import pe.rodcar.redessociales.services.PaqueteService;
+
+@RestController
+@RequestMapping("/paquetes")
+public class PaqueteController {
+
+	@Autowired
+	private PaqueteService paqueteService;
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Paquete>> fetchAll() {
+		try {
+			List<Paquete> paquetes = paqueteService.findAll();
+			if (paquetes.isEmpty()) {
+				return ResponseEntity.noContent().build();
+			}
+
+			return ResponseEntity.ok(paquetes);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().build();
+		}
+	}
+}

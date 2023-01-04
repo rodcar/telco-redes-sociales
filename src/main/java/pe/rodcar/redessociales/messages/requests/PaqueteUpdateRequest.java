4688264a4063da4af8bd0f01a6fad7eb7755a332
rodcar <ivanrod12@hotmail.com>
2023-01-03 23:39:02 -0500

    agrega requests

--- src/main/java/pe/rodcar/redessociales/messages/requests/PaqueteUpdateRequest.java
+++ src/main/java/pe/rodcar/redessociales/messages/requests/PaqueteUpdateRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
-public class PaqueteCreateRequest {
+public class PaqueteUpdateRequest {
 	@NotBlank
 	private String nombre;
 	@NotBlank

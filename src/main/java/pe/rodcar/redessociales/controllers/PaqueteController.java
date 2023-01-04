c0d32fc2e3ab2ae207f5ec7a69eb05579fe9b8f5
rodcar <ivanrod12@hotmail.com>
2023-01-03 23:48:20 -0500

    elimina import no necesario

--- src/main/java/pe/rodcar/redessociales/controllers/PaqueteController.java
+++ src/main/java/pe/rodcar/redessociales/controllers/PaqueteController.java
@@ -2,13 +2,17 @@ package pe.rodcar.redessociales.controllers;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import pe.rodcar.redessociales.entities.Paquete;
 import pe.rodcar.redessociales.messages.requests.PaqueteCreateRequest;
+import pe.rodcar.redessociales.messages.requests.PaqueteUpdateRequest;
 import pe.rodcar.redessociales.services.PaqueteService;
 
 @RestController
@@ -56,6 +61,35 @@ public class PaqueteController {
 			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 					.buildAndExpand(nuevoPaquete.getId()).toUri();
 			return ResponseEntity.created(location).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().build();
+		}
+	}
+
+	@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Paquete> update(@PathVariable("id") Long id,
+			@Validated @RequestBody PaqueteUpdateRequest request) {
+		try {
+			Paquete paquete = Paquete.builder().id(id).nombre(request.getNombre()).descripcion(request.getDescripcion())
+					.precio(request.getPrecio()).build();
+			Paquete paqueteActualizado = paqueteService.update(paquete);
+			return ResponseEntity.ok(paqueteActualizado);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().build();
+		}
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
+		try {
+			if (paqueteService.existsById(id)) {
+				paqueteService.deleteById(id);
+				return ResponseEntity.ok().build();
+			} else {
+				return ResponseEntity.notFound().build();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			return ResponseEntity.internalServerError().build();

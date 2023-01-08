bae0572d2dacfaaf9fdac349993f3dc025824100
rodcar <ivanrod12@hotmail.com>
2023-01-07 23:27:48 -0500

    agrega configuración como cliente de Eureka

--- src/main/java/pe/rodcar/redessociales/TelcoRedesSocialesApplication.java
+++ src/main/java/pe/rodcar/redessociales/TelcoRedesSocialesApplication.java
@@ -2,8 +2,10 @@ package pe.rodcar.redessociales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TelcoRedesSocialesApplication {
 
 	public static void main(String[] args) {
